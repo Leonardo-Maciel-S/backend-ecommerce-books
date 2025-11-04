e00f1f414d68d8eb18f2fcd746b81a229e859697
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-04 14:55:51 -0300

    feat: create route to patch book by id

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,10 +1,13 @@
-import express, { type Request, type Response } from "express";
+import express from "express";
 import { createBook } from "../services/book/create.js";
 import { getAll } from "../services/book/getAll.js";
+import { patchBook } from "../services/book/patch.js";
+import { idValidation } from "../middlewares/idValidations.js";
 
 const bookRoute = express.Router();
 
 bookRoute.get("/", getAll);
 bookRoute.post("/", createBook);
+bookRoute.patch("/:id", idValidation, patchBook);
 
 export { bookRoute };
