e6898549c3d42650465dde5b0a27b3774284c4d7
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-04 20:35:38 -0300

    refactor: change types books to interface folder

---
+++ src/schema/book.ts
@@ -0,0 +1,17 @@
+import yup, { type InferType } from "yup";
+
+export const bookSchema = yup.object({
+  id: yup.string(),
+  title: yup
+    .string()
+    .min(4, "Título precisa ter pelo menos 4 caracteres")
+    .required("Título é obrigatório"),
+  author: yup
+    .string()
+    .min(2, "Autor precisa ter pelo menos 2 caracteres")
+    .required("Autor é obrigatório"),
+  synopsis: yup.string().required("Sinopse é obrigatório"),
+  priceInCents: yup.number().required("Preço é obrigatório"),
+  coverImg: yup.string().required("Imagem é obrigatório"),
+  evaluation: yup.number(),
+});
