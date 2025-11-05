87e5f6647402895b73900008b30c75723501aac6
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-05 12:46:09 -0300

    feat: create router to login user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,5 +4,6 @@ import { userService } from "../services/user/index.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", userService.createUser);
+userRouter.post("/login", userService.login);
 
 export { userRouter };
--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,5 +1,7 @@
 import { createUser } from "./create.js";
+import { login } from "./login.js";
 
 export const userService = {
   createUser,
+  login,
 };
