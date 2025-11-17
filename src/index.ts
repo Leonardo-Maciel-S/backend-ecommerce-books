3ec48334d58c36dd9f33e636ab76a4a7be9036e8
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-17 15:59:31 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import cookieParse from "cookie-parser";
+import cors from "cors";
 
 import { bookRouter } from "./routes/book.js";
 import { userRouter } from "./routes/user.js";
@@ -9,8 +10,9 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(cookieParse());
 
