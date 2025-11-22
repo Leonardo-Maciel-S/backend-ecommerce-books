9e3f490fae0ce1114172954f18d9598ea279f2df
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-22 14:50:41 -0300

    feat: add search by filters in get all

--- src/services/book/get-all.ts
+++ src/services/book/get-all.ts
@@ -2,10 +2,27 @@ import type { Request, Response } from "express";
 import { db } from "../../db/index.js";
 import { bookTable } from "../../db/schema/book.js";
 import status from "http-status";
+import { and, ilike, or } from "drizzle-orm";
 
-export async function getAllBooks(_: Request, res: Response) {
+export async function getAllBooks(req: Request, res: Response) {
+  const search = req.query.search || "";
+
+  const conditions = [];
+
+  if (search) {
+    conditions.push(
+      or(
+        ilike(bookTable.title, `%${search}%`),
+        ilike(bookTable.author, `%${search}%`)
+      )
+    );
+  }
+
   try {
-    const books = await db.select().from(bookTable);
+    const books = await await db
+      .select()
+      .from(bookTable)
+      .where(and(...conditions));
 
     res.status(status.OK).json({ books });
   } catch (error) {
