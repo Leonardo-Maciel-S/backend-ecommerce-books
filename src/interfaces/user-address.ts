aeff16e7191654b2d8d04c28cd137fa4bc93f064
Leonardo Maciel <leonardomacielj@gmail.com>
2025-11-28 13:24:56 -0300

    refactor: omit userID in UserAddressBody type

--- src/interfaces/user-address.ts
+++ src/interfaces/user-address.ts
@@ -2,4 +2,4 @@ import type { InferType } from "yup";
 import type { userAddressSchema } from "../schema/user-address.js";
 
 export type UserAddress = InferType<typeof userAddressSchema>;
-export type UserAddressBody = Omit<UserAddress, "id">;
+export type UserAddressBody = Omit<UserAddress, "id" | "userId">;
