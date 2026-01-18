182423ab561718248431a61a3bee7152f99c2187
Leonardo Maciel <leonardomacielj@gmail.com>
2026-01-18 17:06:29 -0300

    feat: create service and route to get all comment by book id

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { commentService } from "../services/comment/index.js";
 import { loginValidation } from "../middlewares/login-validation.js";
+import { idValidation } from "../middlewares/id-validations.js";
 
 const commentRouter = express.Router();
 
+commentRouter.get("/:id", idValidation, commentService.getAllCommentsByBookId);
 commentRouter.post("/", loginValidation, commentService.createComment);
 
 export { commentRouter };
--- src/services/comment/index.ts
+++ src/services/comment/index.ts
@@ -1,5 +1,7 @@
 import { createComment } from "./create.js";
+import { getAllCommentsByBookId } from "./get-all-by-book-id.js";
 
 export const commentService = {
   createComment,
+  getAllCommentsByBookId,
 };
