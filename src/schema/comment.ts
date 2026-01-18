b7d31a496431508efcd5f38935ece5551210c935
Leonardo Maciel <leonardomacielj@gmail.com>
2026-01-18 16:38:58 -0300

    fix: required fields of comment in interface and schema

--- src/schema/comment.ts
+++ src/schema/comment.ts
@@ -1,10 +1,10 @@
 import yup from "yup";
 
-export const comment = yup.object({
+export const commentSchema = yup.object({
   id: yup.string(),
   userId: yup.string(),
-  bookId: yup.string(),
-  userName: yup.string().min(4, "Nome do usuário é obrigatório"),
-  evaluation: yup.number(),
+  bookId: yup.string().required("Id do livro é obrigatório"),
+  userName: yup.string().required("Nome do usuário é obrigatório"),
+  evaluation: yup.number().required(),
   text: yup.string().required("Comentário é obrigatório"),
 });
