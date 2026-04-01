f66ab2995d74a7613b3af3823aa5d2f2f14f878a
Leonardo Maciel <leonardomacielj@gmail.com>
2026-04-01 16:10:39 -0300

    fix: cartService index import

--- src/services/cart/index.ts
+++ src/services/cart/index.ts
@@ -1,7 +1,7 @@
 import { addCartItem } from "./add-cart-item.js";
 import { decreaseItemCartById } from "./decrease-item-cart-by-id.js";
 import { getAllItemByCartId } from "./get-all-item-by-cart-id.js";
-import { getCartByUserId } from "./get-by-user-id.js";
+import { getCartByUserId } from "./get-cart-by-user-id.js";
 import { incrementItemCartById } from "./increment-item-cart-by-id.js";
 
 export const cartService = {
