26ba3e2c31f58ac5e1f7f62f01a6166b8b09379a
Leonardo Maciel <leonardomacielj@gmail.com>
2026-04-03 21:37:42 -0300

    feat: add item cart check if item already exist

--- src/services/cart/add-cart-item.ts
+++ src/services/cart/add-cart-item.ts
@@ -26,6 +26,17 @@ export async function addCartItem(req: Request<{ id: string }>, res: Response) {
         .json({ message: "Usuário ainda não possui carrinho." });
     }
 
+    const [existItemInCart] = await db
+      .select()
+      .from(cartItemTable)
+      .where(eq(cartItemTable.bookId, id));
+
+    if (existItemInCart) {
+      return res
+        .status(status.BAD_REQUEST)
+        .json({ message: "Item já está no carrinho." });
+    }
+
     const [cartItem] = await db
       .insert(cartItemTable)
       .values({
